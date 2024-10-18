4f10523a213c318b94b27903796bf6b3e08d07b0
Sagar <sagar@wipl.co.in>
2024-10-18 17:04:33 +0530

    add some configure of express and utils class

--- src/db/index.js
+++ src/db/index.js
@@ -12,13 +12,12 @@ const connectDB = async () => {
   });
 
   try {
-    // This will attempt to get a client from the pool to verify the connection
     await pool.connect();
     console.log("Connected to the PostgreSQL database!");
-    return pool; // Return the pool instance for further use
+    return pool;
   } catch (error) {
     console.error("PostgreSQL connection FAILED:", error);
-    process.exit(1); // Exit the process with an error code
+    process.exit(1); 
   }
 };
 
